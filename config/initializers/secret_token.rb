8a530e8e4904478665468b4cc8569b5770cee90b
valikos <valikos.ost@gmail.com>
2013-03-24 19:17:30 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeOnRails::Application.config.secret_token = '9bd852a0cf75b96ce15abff5fcfbd6d4ff9e9dcb9b0228264a23bb623080051d8f74d4731df77cb0edf195129a84a857d79821c8a4079feb5c85504d5f1efd8e'
